9f2f2bce75177087d6b927cc4d331f690830f3a4
amitchandradas2004 <amitcdas792@gmail.com>
2026-07-13 17:10:01 +0600

    serverside is connected to Mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
-
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI as string;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("API is running");
+  res.send("Welcome to the ScholarBridge API!");
 });
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
